e068bdc313b0e79c7710df48f5cc1188865fb5e9
princes22334 <princes22334@gmail.com>
2024-05-02 22:20:59 +0530

    first commit

---
+++ Basics/HelloWorld.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class HelloWorld{
+    public static void main(String[] args){
+       // System.out.println("Hello World");
+        System.out.println("Hello Prince");
+        System.out.print("Hello, Ankita");
+
+        }
+}
